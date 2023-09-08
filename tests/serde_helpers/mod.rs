09ccf4986f37234838d70b2869a438193ec630ef
Mingun <alexander_sergey@mail.ru>
2023-09-08 22:33:20 +0500

    Implement Deserializer trait for DOM nodes

--- tests/serde_helpers/mod.rs
+++ tests/serde_helpers/mod.rs
@@ -3,14 +3,16 @@
 use pretty_assertions::assert_eq;
 use quick_xml::DeError;
 use quick_xml::de::Deserializer;
+use quick_xml::reader::dom::Element;
 use serde::Deserialize;
+use std::fmt::Debug;
 
 /// Deserialize an instance of type T from a string of XML text.
 /// If deserialization was succeeded checks that all XML events was consumed
 #[track_caller]
 pub fn from_str<'de, T>(source: &'de str) -> Result<T, DeError>
 where
-    T: Deserialize<'de>,
+    T: Deserialize<'de> + PartialEq + Debug,
 {
     // Log XML that we try to deserialize to see it in the failed tests output
     dbg!(source);
@@ -24,17 +26,33 @@ where
 
     let result = T::deserialize(&mut de);
 
-    // If type was deserialized, the whole XML document should be consumed
-    if result.is_ok() {
-        de.check_eof_reached();
+    let dom = Element::from_str(source);
 
-        let resolver = de.resolver();
-        assert_eq!(
-            resolver.level(),
-            0,
-            "all namespace bindings must be popped, level should be zero: {:#?}",
-            resolver
-        );
+    match result {
+        // If type was deserialized, the whole XML document should be consumed
+        Ok(ref de_result) => {
+            de.check_eof_reached();
+
+            let resolver = de.resolver();
+            assert_eq!(
+                resolver.level(),
+                0,
+                "all namespace bindings must be popped, level should be zero: {:#?}",
+                resolver
+            );
+
+            let dom = dom.expect("DOM parsing should succeed when type is deserialized");
+            let dom_result = T::deserialize(dbg!(dom))
+                .expect("DOM deserialization should succeed when type is deserialized");
+            assert_eq!(&dom_result, de_result);
+        }
+        Err(_) => {
+            if let Ok(dom) = dom {
+                if let Ok(dom_result) = T::deserialize(dbg!(dom)) {
+                    panic!("DOM deserialization should fail, but got: {:?}", dom_result);
+                }
+            }
+        }
     }
 
     result
